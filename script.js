2a6204899ba675696dcb3f1142dee1ef78d3ca92
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-19 00:20:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function createTable() {
-    //Write your code here
-  
+    //Write your 
+  var rowCount = parseInt(prompt('Input number of rows'));
+  var columnCount = parseInt(prompt('Input number of columns'));
+
+  var table = document.getElementById('myTable');
+  table.innerHTML = '';
+
+  for (var i = 0; i < rowCount; i++) {
+    var row = document.createElement('tr');
+
+    for (var j = 0; j < columnCount; j++) {
+      var cell = document.createElement('td');
+      cell.textContent = 'Row-' + i + ' Column-' + j;
+      row.appendChild(cell);
+    }
+
+    table.appendChild(row);
+  }
 }
+
+  
+
